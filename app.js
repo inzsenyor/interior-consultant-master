845e72d23bc96a74cf448bd719f2327a8e2eac28
inzsenyor <49244039+inzsenyor@users.noreply.github.com>
2020-11-27 14:44:37 +0100

    style reworked

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ hambiBtn.addEventListener('click', function (e) {
   nav.classList.add('open');
   hambiBtn.style.display = 'none';
   closeBtn.style.display = 'block';
+  document.body.style.overflowY = 'hidden';
 });
 
 closeBtn.addEventListener('click', function (e) {
   nav.classList.remove('open');
   hambiBtn.style.display = 'block';
   closeBtn.style.display = 'none';
+  document.body.style.overflowY = 'scroll';
 });
