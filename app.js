d9a59acdc225e28fdf4d402008ab368cd9ff7154
inzsenyor <49244039+inzsenyor@users.noreply.github.com>
2020-11-27 16:19:24 +0100

    js changed

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const hambiBtn = document.querySelector('.hamburger');
 const closeBtn = document.querySelector('.close');
 const nav = document.querySelector('.navigation');
 
+document.getElementById('div').style.pointerEvents = 'none';
+
 hambiBtn.addEventListener('click', function (e) {
   nav.classList.add('open');
   hambiBtn.style.display = 'none';
   closeBtn.style.display = 'block';
-  document.body.style.overflow = 'hidden';
+  document.body.style.overflowY = 'hidden';
+  document.body.style.pointerEvents = 'none';
 });
 
 closeBtn.addEventListener('click', function (e) {
   nav.classList.remove('open');
   hambiBtn.style.display = 'block';
   closeBtn.style.display = 'none';
-  document.body.style.overflow = 'scroll';
+  document.body.style.overflowY = 'scroll';
+  document.body.style.pointerEvents = 'auto';
 });
